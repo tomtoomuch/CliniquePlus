8f9134796d407c9ae8c94939039b82f322dd6ae7
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-10 09:29:40 +0200

    route 2

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 const express = require("express");
 // Le framework est chargé dans app
 const app = express();
+const port = 3000;
 
+// On simule une BDD grâce à ce tableau d'utilisateurs
+const users = [
+    { mail: "Alice@gmail.com", id: 0, pass: "azerty" },
+    { mail: "Bob@gmail.com", id: 1, pass: "qwerty" },
+    { mail: "Charlie@gmail.com", id: 2, pass: "qwertz" },
+]
+// Premier contact
+app.get('/handshake', (req, res) => {
+  res.send('Hello World!');
+});
 
-
-
+app.get('/login', (req,res) => {
+    res.send('Login - TODO');
+});
 
 // On lance l'exécution du serveur - IMPORTANT, le lancement du serveur est touujours en dernier.
 app.listen(3000, () => {
