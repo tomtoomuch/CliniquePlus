62d7be2541256b5822e0fc1cbe4f6f9c429d0f32
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-10 11:58:45 +0200

    rien de très bon

--- index.js
+++ index.js
@@ -2,29 +2,31 @@
 const express = require("express");
 // Le framework est chargé dans app
 const app = express();
+// On conditionne le framework pour l'usage du json
+app.use(express.json());
+// On déclare une variable qui contient le port
 const port = 3000;
 
 // On simule une BDD grâce à ce tableau d'utilisateurs
 const users = [
     { mail: "Alice@gmail.com", id: 0, pass: "azerty" },
     { mail: "Bob@gmail.com", id: 1, pass: "qwerty" },
-    { mail: "Charlie@gmail.com", id: 2, pass: "qwertz" },
-]
+    { mail: "Charlie@gmail.com", id: 2, pass: "qwertz" }
+];
 // Premier contact
 app.get('/handshake', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/login', (req,res) => {
-    
-    res.send('Login - TODO');
+app.post('/login', (req,res) => {
+    const { mail , password } = req.body;
+    console.log(mail);
 });
-
 app.get('/admin', (req,res) => {
 
 });
 
 // On lance l'exécution du serveur - IMPORTANT, le lancement du serveur est touujours en dernier.
-app.listen(3000, () => {
-    console.log("Serveur démarré sur le port 3000");
+app.listen(port, (port) => {
+    console.log("Serveur démarré sur le port ", port);
 });
